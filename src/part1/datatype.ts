ecca401e4aa8b41a209cf51e698e3b6963415262
ludongfang <ldf_5566@163.com>
2024-07-02 17:41:50 +0800

    增加示例代码

--- src/part1/datatype.ts
+++ src/part1/datatype.ts
@@ -12,3 +12,46 @@ let tuple: [number, string] = [1, '3']
 // tuple.push(2)
 // console.log(tuple)
 // tuple[2]
+
+// 函数
+let add = (x: number, y: number) => x + y
+let compute: (x: number, y: number) => number
+
+compute = (a, b) => a + b
+
+// 对象
+let obj: { x: number, y: number } = { x: 1, y: 2 }
+obj.x = 3
+
+// symbol 
+let s1: symbol = Symbol()
+let s2 = Symbol()
+
+console.log(s1 == s2)
+
+// undefined null
+
+let un: undefined = undefined
+let nu: null = null
+num = null
+num = undefined
+
+// void
+let noReturn = () => { }
+
+// any
+let x
+
+x = 1
+x = []
+x = () => { }
+
+//never
+let error = () => {
+    throw new Error('error')
+}
+
+let endless = () => {
+    while (true) { }
+}
+
